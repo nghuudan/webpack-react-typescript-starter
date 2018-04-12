47a829803c12f02dbc5e60f88c904cabcfa8080f
Dan Nguyen <nghuudan@gmail.com>
2018-04-11 23:27:38 -0400

    Add AppProps interface stub

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,5 +1,7 @@
 import React, { StatelessComponent } from 'react';
 
-const App = () => <h1>Hello React with TypeScript!</h1>;
+export interface AppProps {}
+
+const App: StatelessComponent<AppProps> = () => <h1>Hello React with TypeScript!</h1>;
 
 export default App;
