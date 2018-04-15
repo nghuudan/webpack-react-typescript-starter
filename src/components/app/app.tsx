cb48e35c2734b28e95f3b6e48c67d5bbfc257f51
Dan Nguyen <nghuudan@gmail.com>
2018-04-14 22:28:12 -0400

    Add basic styles

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,6 +2,8 @@ import React, { StatelessComponent } from 'react';
 
 export interface AppProps {}
 
-const App: StatelessComponent<AppProps> = () => <h1>Hello React with TypeScript!</h1>;
+const App: StatelessComponent<AppProps> = () => (
+  <h1 className="dn-app-heading">Hello React with TypeScript!</h1>
+);
 
 export default App;
